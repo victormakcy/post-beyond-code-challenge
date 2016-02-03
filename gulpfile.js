dbce5d4cd8b785f5e3a0aeffe30bf8025515c581
Victor Mak <victormakcy@gmail.com>
2016-02-02 22:16:23 -0500

    Setup gulp file

---
+++ gulpfile.js
@@ -0,0 +1,13 @@
+var gulp = require('gulp'),
+    bower = require('gulp-bower');
+ 
+var config = {
+     bowerDir: './bower_components'
+}
+ 
+gulp.task('bower', function() {
+    return bower()
+        .pipe(gulp.dest(config.bowerDir))
+});
+ 
+gulp.task('default', ['bower']);
