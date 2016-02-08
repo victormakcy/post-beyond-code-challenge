98afa3c1f50dd4b0b6ff16b60f6347665251ff2e
Victor <victormakcy@gmail.com>
2016-02-07 23:01:00 -0500

    moving api calls to server side

--- app.js
+++ app.js
@@ -1,66 +1,47 @@
 var PBApp = angular.module('PBApp', []);
 
-PBApp.controller('BookmarksController', ['$scope', '$http',
-    function($scope, $http) {
+PBApp.controller('BookmarksController', ['$scope', '$http', function($scope, $http) {
 
-        getAccessToken();
+  $scope.init = function() {
+    $scope.getBookmarks();
+  }
 
-        function getAccessToken() {
-            var httpMethod = 'GET',
-                url = 'https://www.readability.com/api/rest/v1/oauth/access_token/',
-                timestamp = Date.now(),
-                parameters = {
-                    oauth_consumer_key: 'victormakcy',
-                    oauth_nonce: 'kllo9940pd9333jh',
-                    oauth_timestamp: timestamp,
-                    oauth_signature_method: 'HMAC-SHA1',
-                    oauth_version: '1.0',
-                    x_auth_username: 'victormakcy',
-                    x_auth_password: '123456',
-                    x_auth_mode: 'client_auth'
-                },
-                consumerSecret = '5feEMqAWPmCqwS9BHsHME6dck9zEP5GL',
-                // generates a RFC 3986 encoded, BASE64 encoded HMAC-SHA1 hash
-                encodedSignature = oauthSignature.generate(httpMethod, url, parameters, consumerSecret);
+  $scope.getBookmarks = function() {
+      $scope.bookmarks = [];
+       var httpMethod = 'POST',
+          url = 'https://www.readability.com/api/rest/v1/bookmarks',
+          timestamp = Date.now(),
+          oauth = {
+            oauth_consumer_key: 'victormakcy',
+            oauth_nonce: 'kllo9940pd9333jh',
+            oauth_timestamp: timestamp,
+            oauth_signature_method: 'HMAC-SHA1',
+            oauth_version: '1.0',
+            oauth_token: '6Sk6ATeW5mXKKbjVDg',
+            oauth_token_secret: 'g3jxNbgUM8Rb7pmHPEhKf3gPcLNwkBXt'
+          };
+          consumerSecret = '5feEMqAWPmCqwS9BHsHME6dck9zEP5GL',
+          encodedSignature = oauthSignature.generate(httpMethod, url, oauth, consumerSecret);
+          oauth.oauth_signature = encodedSignature;
 
-            parameters.oauth_signature = encodedSignature;
+      $http({
+          method: 'POST',
+          url: url,
+          params: oauth,
+      }).then(function (success) {
+          console.log(success.data);
+          // this callback will be called asynchronously
+          // when the response is available
+      });
+  }
 
-            $http({
-                method: 'JSONP',
-                url: url,
-                params: parameters
-            }).then(function (response) {
-                // this callback will be called asynchronously
-                // when the response is available
-            }, function errorCallback(response) {
-                // called asynchronously if an error occurs
-                // or server returns response with an error status.
-            });
-        }
+  $scope.displayBookmarks = function (data) {
+      for (var i = 0; i < data.length; i++) {
 
-        function getBookmarks() {
-            $scope.bookmarks = [];
-
-            $http({
-                method: 'jsonp',
-                url: 'https://www.readability.com/api/rest/v1/bookmarks',
-                params: parameters
-            }).then(function (response) {
-                // this callback will be called asynchronously
-                // when the response is available
-            }, function errorCallback(response) {
-                // called asynchronously if an error occurs
-                // or server returns response with an error status.
-            });
-        }
-
-        $scope.displayBookmarks = function (data) {
-            for (var i = 0; i < data.length; i++) {
- 
-                // add each bookmark info to bookmarks array
-                $scope.bookmarks.push({
-                    // specific data to go here
-                });
-            }
-        }
-    }])
+          // add each bookmark info to bookmarks array
+          $scope.bookmarks.push({
+              // specific data to go here
+          });
+      }
+  }
+}])
---
+++ server.js
@@ -0,0 +1,60 @@
+var express = require('express');
+var request = require('request');
+var config = require('./config');
+var qs = require('querystring');
+var app = express();
+
+var accessTokens = {};
+
+app.get('/', function (req, res) {
+  requestAccessToken(function (err, tokens) {
+    accessTokens = tokens;
+
+    requestBookmarks(accessTokens, function (bookmarks) {
+      res.send(bookmarks);
+    })
+  });
+});
+
+requestAccessToken = function (cb) {
+  var conf = config.get(),
+      httpOptions = {
+        oauth: {
+          consumer_key: conf.consumer_key,
+          consumer_secret: conf.consumer_secret
+        },
+        qs: {
+          x_auth_username: conf.x_auth_username,
+          x_auth_password: conf.x_auth_password,
+          x_auth_mode: conf.x_auth_mode,
+        }
+      };
+
+  request.get('https://www.readability.com/api/rest/v1/oauth/access_token/', httpOptions, function(err, response, body) {
+    var tokens = qs.parse(body);
+    delete tokens.oauth_callback_confirmed;
+
+    return cb(null, tokens);
+  });
+}
+
+requestBookmarks = function (tokens, cb) {
+  console.log(tokens);
+  var conf = config.get(),
+      httpOptions = {
+        oauth: {
+          consumer_key: conf.consumer_key,
+          consumer_secret: conf.consumer_secret,
+          token: tokens.oauth_token,
+          token_secret: tokens.oauth_token_secret
+        }
+      };
+
+  request.get('https://www.readability.com/api/rest/v1/bookmarks', httpOptions, function(err, response, body) {
+    return cb(JSON.parse(body));
+  });
+}
+
+app.listen(3000, function () {
+  console.log('Example');
+});
