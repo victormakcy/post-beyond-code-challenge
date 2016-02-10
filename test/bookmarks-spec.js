4d6503f4f1f0fc3bff72b9bf5e47e9fa47a74876
Victor <victormakcy@gmail.com>
2016-02-10 11:03:00 -0500

    adding two tests

---
+++ test/bookmarks-spec.js
@@ -0,0 +1,20 @@
+describe('post beyond code challenge', function() {
+  it('bookmarks home page should have a nav-bar with an add bookmark button', function() {
+    browser.get('http://localhost:3000');
+
+    expect(element(by.className('js-nav-bar')).isPresent()).toBe(true);
+    expect(element(by.className('js-add-bookmarks-button')).isPresent()).toBe(true);
+  });
+
+  it('bookmarks home page should have a nav-bar with an add bookmark button', function() {
+    browser.get('http://localhost:3000');
+
+    var bookmarks = element.all(by.repeater('bookmark in bookmarks'));
+    var articleCardTitle = bookmarks.get(0).element(by.className('js-article-card__title')).getText();
+
+    bookmarks.get(0).click();
+
+    expect(element(by.className('js-article__title')).isPresent()).toBe(true);
+    expect(element(by.className('js-article__title')).getText()).toEqual(articleCardTitle);
+  });
+});
